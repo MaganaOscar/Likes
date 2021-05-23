434e772f6bab3127c6e0ae2f973240557c3c8f49
MaganaOscar <oscmag13@gmail.com>
2021-05-23 15:39:30 -0700

    finished part II

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-var count = 0;
-var countElement = document.querySelector("#likes");
+var count1 = 0;
+var count2 = 0;
+var count3 = 0;
+var countElement1 = document.querySelector("#likes1");
+var countElement2 = document.querySelector("#likes2");
+var countElement3 = document.querySelector("#likes3");
 
-function liked () {
-    count++;
-    countElement.innerText = count.toString() + " like(s)";
+function liked1 () {
+    count1++;
+    countElement1.innerText = count1 + " like(s)";
+}
+function liked2 () {
+    count2++;
+    countElement2.innerText = count2 + " like(s)";
+}
+function liked3 () {
+    count3++;
+    countElement3.innerText = count3 + " like(s)";
 }
